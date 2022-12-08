6775ae1e34346a369411e4d8e39f5c9db418c2c8
Yasir284 <lambawalay@gmail.com>
2022-12-08 21:03:59 +0530

    Added user context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { ThemeContext } from "./context/ThemeContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+
+// Contexts
+import { ThemeContext } from "./context/ThemeContext";
+import { UserContext } from "./context/UserContext";
 
 // Components
 import MainSection from "./components/MainSection";
@@ -10,8 +14,12 @@ import Navbar from "./components/Navbar";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 
+axios.defaults.baseURL = "http://localhost:4001";
+axios.defaults.withCredentials = true;
+
 function App() {
   const [theme, setTheme] = useState("light");
+  const [isSignedIn, setIsSignedIn] = useState(null);
 
   useEffect(() => {
     if (theme === "dark") {
@@ -21,22 +29,41 @@ function App() {
     }
   }, [theme]);
 
+  // Check if user is signed in
+  const checkIsSignedIn = async () => {
+    const res = await axios
+      .get("/todo/u/isSignedIn")
+      .catch((error) => error.response);
+    console.log(res);
+
+    if (!res.data.success) {
+      return setIsSignedIn(false);
+    }
+    setIsSignedIn(true);
+  };
+
+  useEffect(() => {
+    checkIsSignedIn();
+  }, [setIsSignedIn]);
+
   return (
     <>
-      <ThemeContext.Provider value={{ theme, setTheme }}>
-        <ToastContainer
-          position="top-right"
-          autoClose={2000}
-          theme={theme === "dark" ? "dark" : "light"}
-        />
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Navigate replace to="/todo" />} />
-          <Route path="/todo" element={<MainSection />} />
-          <Route path="/signUp" element={<SignUp />} />
-          <Route path="/signIn" element={<SignIn />} />
-        </Routes>
-      </ThemeContext.Provider>
+      <UserContext.Provider value={{ isSignedIn, setIsSignedIn }}>
+        <ThemeContext.Provider value={{ theme, setTheme }}>
+          <ToastContainer
+            position="top-right"
+            autoClose={2000}
+            theme={theme === "dark" ? "dark" : "light"}
+          />
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Navigate replace to="/todo" />} />
+            <Route path="/todo" element={<MainSection />} />
+            <Route path="/signUp" element={<SignUp />} />
+            <Route path="/signIn" element={<SignIn />} />
+          </Routes>
+        </ThemeContext.Provider>
+      </UserContext.Provider>
     </>
   );
 }
